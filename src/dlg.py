a429da3b1d4357393e019e574ed3e29ac10c542c
Ronan Murphy <ronan.murphy@originmarkets.com>
2019-05-28 14:03:22 +0100

    Implementing total endpoint + more tests.

--- src/dlg.py
+++ src/dlg.py
@@ -1,9 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
+def invalid_request(reason, status_code=400):
+    resp = jsonify({"message": reason})
+    resp.status_code = status_code
+    return resp
+
+
 @app.route("/total/", methods=["POST"])
 def total():
-    result = {"total": 0}
-    return jsonify(result)
+    if not request.is_json:
+        return invalid_request("Was expecting a mimetype of application/json")
+
+    numbers_to_sum = request.get_json()
+
+    if not isinstance(numbers_to_sum, list):
+        return invalid_request(
+            f"Was expecting a list. Given: {type(numbers_to_sum).__name__}"
+        )
+
+    try:
+        total = sum(numbers_to_sum)
+    except TypeError:
+        return invalid_request(
+            "Provided list of numbers contains a non-numeric element."
+        )
+
+    return jsonify({"total": total})
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -7,8 +7,62 @@ class APITests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.client = app.test_client()
-        app.config['TESTING'] = True
+        app.testing = True
 
     def test_total_endpoint_exists(self):
         resp = self.client.post("/total/")
         assert resp.status_code != 404
+
+    def test_no_json_content(self):
+        resp = self.client.post("/total/")
+        assert resp.status_code == 400
+        assert resp.get_json() == {
+            "message": "Was expecting a mimetype of application/json"
+        }
+
+    def test_given_a_string_instead_of_list(self):
+        resp = self.client.post("/total/", json="Not a list of numbers")
+        assert resp.status_code == 400
+        assert resp.get_json() == {
+            "message": "Was expecting a list. Given: str"
+        }
+
+    def test_given_a_dict_instead_of_list(self):
+        resp = self.client.post("/total/", json={"Not": "a list"})
+        assert resp.status_code == 400
+        assert resp.get_json() == {
+            "message": "Was expecting a list. Given: dict"
+        }
+
+    def test_not_all_elements_are_numbers(self):
+        resp = self.client.post("/total/", json=[1, 2, "3"])
+        assert resp.status_code == 400
+        assert resp.get_json() == {
+            "message": "Provided list of numbers contains a non-numeric element."
+        }
+
+    def test_empty_list(self):
+        resp = self.client.post("/total/", json=[])
+        assert resp.status_code == 200
+        assert resp.get_json()["total"] == 0
+
+    def test_small_list_of_numbers(self):
+        resp = self.client.post("/total/", json=[1, 2, 3])
+        assert resp.status_code == 200
+        assert resp.get_json()["total"] == 6
+
+    def test_large_list_of_number(self):
+        numbers = list(range(10000001))
+        resp = self.client.post("/total/", json=numbers)
+        assert resp.status_code == 200
+        assert resp.get_json()["total"] == 50000005000000
+
+    def test_negative_numbers(self):
+        resp = self.client.post("/total/", json=[1, -2, 3])
+        assert resp.status_code == 200
+        assert resp.get_json()["total"] == 2
+
+    def test_floats(self):
+        resp = self.client.post("/total/", json=[1, 2.5, 3])
+        assert resp.status_code == 200
+        assert resp.get_json()["total"] == 6.5
